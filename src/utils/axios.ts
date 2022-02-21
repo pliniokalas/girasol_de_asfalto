af2cd70057ad0d519b0b598a9f0346eeaad911bd
Plinio Kalas <pliniokalas@users.noreply.github.com>
2022-02-21 11:31:06 -0300

    Update baseUrl

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
 import { getSessionCookie } from 'utils/cookies';
 
-axios.defaults.baseURL = 'http://localhost:8445';
+// axios.defaults.baseURL = 'http://localhost:8445';
+axios.defaults.baseURL = 'https://girassol-de-asfalto.herokuapp.com';
 
 axios.interceptors.request.use(function (config) {
   const token = getSessionCookie(); 
