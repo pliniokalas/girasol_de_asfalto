63c01846b2ac118a0086dbe51b63c0c1f0cfdc72
Plinio Kalas <pliniokalas@users.noreply.github.com>
2022-02-21 11:59:45 -0300

    Add conditional baseUrl

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -1,8 +1,9 @@
 import axios from 'axios';
 import { getSessionCookie } from 'utils/cookies';
 
-// axios.defaults.baseURL = 'http://localhost:8445';
-axios.defaults.baseURL = 'https://girassol-de-asfalto.herokuapp.com';
+axios.defaults.baseURL = process.env.NODE_ENV === 'production'
+  ? 'https://girassol-de-asfalto.herokuapp.com'
+  : 'http://localhost:8445';
 
 axios.interceptors.request.use(function (config) {
   const token = getSessionCookie(); 
